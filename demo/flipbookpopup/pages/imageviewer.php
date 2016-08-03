46f6597f01a4a00a8f2d57f5427a8ab82541a832
Duncan Tang <dun.tang@yahoo.com>
2016-08-03 13:28:30 +0800

    Add files via upload

---
+++ demo/flipbookpopup/pages/imageviewer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<HTML>
+<HEAD>
+<TITLE>Hang Lung Brochure</TITLE>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style type="text/css">
+<!--
+body {
+	background-color: #333333;
+	margin-left: 0px;
+	margin-top: 20px;
+	margin-right: 0px;
+	margin-bottom: 20px;
+}
+-->
+</style></HEAD>
+<BODY><div align="center">
+<a href='javascript:self.close();'> <img src='<?php echo $image; ?>' border="0"></a>
+</div>
+</BODY>
+</HTML>
